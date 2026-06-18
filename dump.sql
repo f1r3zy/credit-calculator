43849eccc29d288ab88c822f956462537ecf358f
f1r3zy <f1r3zy@gmail.com>
2026-06-18 16:26:53 +0300

    Adaugare pagini statice, actualizare footer, dump.sql curatat

--- dump.sql
+++ dump.sql
@@ -1,31 +1,9 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Хост: 127.0.0.1
--- Время создания: Май 17 2026 г., 19:11
--- Версия сервера: 10.4.32-MariaDB
--- Версия PHP: 8.2.12
+CREATE DATABASE IF NOT EXISTS credit_calculator CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE credit_calculator;
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- База данных: `credit_calculator`
---
-
--- --------------------------------------------------------
-
---
--- Структура таблицы `reference_rates`
---
 
 CREATE TABLE `reference_rates` (
   `id` int(11) NOT NULL,
@@ -35,12 +13,6 @@ CREATE TABLE `reference_rates` (
   `overnight_deposit` decimal(5,2) DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Структура таблицы `simulations`
---
 
 CREATE TABLE `simulations` (
   `id` int(11) NOT NULL,
@@ -52,12 +24,6 @@ CREATE TABLE `simulations` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- --------------------------------------------------------
-
---
--- Структура таблицы `users`
---
-
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `name` varchar(100) NOT NULL,
@@ -66,64 +32,28 @@ CREATE TABLE `users` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Индексы сохранённых таблиц
---
-
---
--- Индексы таблицы `reference_rates`
---
 ALTER TABLE `reference_rates`
   ADD PRIMARY KEY (`id`);
 
---
--- Индексы таблицы `simulations`
---
 ALTER TABLE `simulations`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `share_token` (`share_token`),
   ADD KEY `user_id` (`user_id`);
 
---
--- Индексы таблицы `users`
---
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `email` (`email`);
 
---
--- AUTO_INCREMENT для сохранённых таблиц
---
-
---
--- AUTO_INCREMENT для таблицы `reference_rates`
---
 ALTER TABLE `reference_rates`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT для таблицы `simulations`
---
 ALTER TABLE `simulations`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT для таблицы `users`
---
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- Ограничения внешнего ключа сохраненных таблиц
---
-
---
--- Ограничения внешнего ключа таблицы `simulations`
---
 ALTER TABLE `simulations`
   ADD CONSTRAINT `simulations_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE SET NULL;
-COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+COMMIT;
